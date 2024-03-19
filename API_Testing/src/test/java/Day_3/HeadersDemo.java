1caf7345a3c56ab91af5bfffaf1554c986e5a85d
Vasif Sultanov <cdm_89@mail.ru>
2024-03-19 16:45:14 +0500

    new 19.03.2024

--- API_Testing/src/test/java/Day_3/HeadersDemo.java
+++ API_Testing/src/test/java/Day_3/HeadersDemo.java
@@ -1,8 +1,9 @@
 package Day_3;
 
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.given;
 
 
@@ -14,7 +15,7 @@ public class HeadersDemo {
                 .when()
                 .get("https://www.google.com/")
                 .then()
-                .header("Content-Type", "text/html; charset=ISO-8859-1")
+                .header("Content-Type", "text/html; charset=ISO-8859-1")// валидация хедеров
                 .and()// используется для объединения различных условий или действий в одной цепочке - не обязательно!
                 .header("Content-Encoding", "gzip")
                 .and()
@@ -22,7 +23,7 @@ public class HeadersDemo {
 
     }
 
-    @Test(priority = 2)
+  //  @Test(priority = 2)
     void getHeader() {
 
         Response res = given()
@@ -30,9 +31,33 @@ public class HeadersDemo {
                 .get("https://www.google.com/");
 
         //get single header info
-
-        String headerValue = res.getHeader("Content-Type");
+        String headerValue = res.getHeader("Content-Type"); // получение значение хедера "Content-Type"
         System.out.println("The value of the Content-type is: " + headerValue);
+
+        //get all headers info
+
+        Headers myHeaders = res.getHeaders();
+        for (Header hd : myHeaders) {
+            System.out.println();
+            System.out.println(hd.getName()+ "        "+hd.getValue());
+        }
+
+    }
+
+    @Test(priority = 3)
+    void getHeader2() {
+
+        Response res = given()
+                .when()
+                .get("https://mobapi-dev.fincube.uz/api/card/");
+
+
+        //get all headers info
+
+        Headers myHeaders = res.getHeaders();
+        for (Header hd : myHeaders) {
+            System.out.println(hd.getName()+ "        "+hd.getValue());
+        }
 
     }
 
