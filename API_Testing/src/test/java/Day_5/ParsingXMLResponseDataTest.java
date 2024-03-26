30fb5f196ff1d976e3f5847682dd3c244793d17b
Vasif Sultanov <cdm_89@mail.ru>
2024-03-26 17:19:40 +0500

    new 26.03.2024

--- API_Testing/src/test/java/Day_5/ParsingXMLResponseDataTest.java
+++ API_Testing/src/test/java/Day_5/ParsingXMLResponseDataTest.java
@@ -4,6 +4,7 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -11,44 +12,39 @@ import org.testng.annotations.Test;
 import java.io.*;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static io.restassured.RestAssured.given;
 
 
 public class ParsingXMLResponseDataTest {
-    private WireMockServer wireMockServer;
-
-    @BeforeTest
-    public void setup() {
-        wireMockServer = new WireMockServer(); // Создание нового сервера
-        wireMockServer.start(); // Запуск сервера
-        WireMock.configureFor("localhost", wireMockServer.port()); // Настройка клиента на работу с сервером
-    }
-
-    @AfterTest
-    public void teardown() {
-        wireMockServer.stop(); // Остановка сервера
-    }
 
     @Test
-    void test() {
-/*        File f = new File("D:/0.Practice/API_Automation/API_Testing/JsonFiles/travelers.xml");
-        StringBuffer stringBuffer = new StringBuffer();
-        try (BufferedReader br = new BufferedReader(new FileReader(f))) {
-            // Читаем содержимое файла построчно
-            String line;
-            while ((line = br.readLine()) != null) {
-                stringBuffer.append(line).append("\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println(stringBuffer);*/
+    void testXMLResponse() {
+        //Approach 1
 
-        stubFor(get(urlEqualTo(""))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withBodyFile("D:/0.Practice/API_Automation/API_Testing/JsonFiles/travelers.xml")));
-        //.withHeader("Content-Type", "application/xml")
-        // .withBody("<YourXmlResponseObject><field1>Value1</field1><field2>Value2</field2></YourXmlResponseObject>")));
+        /*given()
+                .when()
+                .get("http://restapi.adequateshop.com/api/Traveler?page=1")
+                .then()
+                .statusCode(200)
+                .header("Content-type", "application/xml")
+                .body("TravelerinformationResponse.page", equalTo("1"))
+                .body("TravelerinformationResponse.travelers.Travelerinformation[0].name", equalTo("Vijay Bharath Reddy"));*/
+
+        Response res = given()
+                .when()
+                .get("http://restapi.adequateshop.com/api/Traveler?page=1");
+
+        Assert.assertEquals(res.getStatusCode(),200);
+        Assert.assertEquals(res.header("Content-type"),"application/xml");
+
+        String pageNo = res.xmlPath().get("TravelerinformationResponse.page").toString();
+
+
+
+        Assert.assertEquals(res.xmlPath().get("TravelerinformationResponse.page").toString().body("Content-type"),"application/xml");
+
+
+
 
     }
 }
